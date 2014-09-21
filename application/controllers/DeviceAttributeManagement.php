14d99ee483ec7683a6ec3b80a868abaeba96e6f8
Mohammed Manssour <man.mhd@gmail.com>
2014-09-21 12:38:06 +0300

    added the controller

--- application/controllers/DeviceAttributeManagement.php
+++ application/controllers/DeviceAttributeManagement.php
@@ -1,13 +1,24 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
- * Created by JetBrains PhpStorm.
- * User: Albert
- * Date: 9/14/14
- * Time: 4:09 PM
- * To change this template use File | Settings | File Templates.
+ * application/controllers/DeviceAttributeManagement.php main controller for managing device atributes
+ * Copyright (C) 2008-2012 Brain Socker <berainsocket.com>
+ *
+ * LICENSE: this program isn't open source
+ * you don't have the right to copy it,use it, download it or use some part of it without
+ * permission
+ *
+ * @package Clix
+ * @version 0.1
+ * @author  samer shatta <email@example.com>
+ * @author  Mohammed Manssour <manssour.mohammed@gmail.com>
+ * @link    http://www.example.com
  */
-
 class DeviceAttributeManagement extends MY_Controller
 {
-
+	function __construct(){
+		parent::__construct();
+        $this->load->model('deviceModel');
+        $this->load->model('LanguageModel');
+        $this->imagesDestPath='images/news/';
+	}
 }
